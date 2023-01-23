0bbee5ad142ae28eaeecf3dd81c2540ab4288e7d
Thales <thales.souz@outlook.com>
2023-01-23 16:02:33 -0300

    fix: implements get_max_salary function - req 04

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -1,22 +1,15 @@
 from typing import Union, List, Dict
+from .jobs import read
 
 
 def get_max_salary(path: str) -> int:
-    """Get the maximum salary of all jobs
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    int
-        The maximum salary paid out of all job opportunities
-    """
-    raise NotImplementedError
+    jobs_list = read(path)
+    max_salary = int()
+    for job in jobs_list:
+        if len(job["max_salary"]) > 0 and job["max_salary"] != 'invalid':
+            if max_salary < int(job["max_salary"]):
+                max_salary = int(job["max_salary"])
+    return max_salary
 
 
 def get_min_salary(path: str) -> int:
