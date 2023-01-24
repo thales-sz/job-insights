eea3947afecfeb27477875167ae63b6fc491b395
Thales <thales.souz@outlook.com>
2023-01-24 15:27:51 -0300

    test: counter test - req 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,6 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    assert count_ocurrences("data/jobs.csv", "Remote") == 352
+    assert count_ocurrences("data/jobs.csv", "On site") == 19
