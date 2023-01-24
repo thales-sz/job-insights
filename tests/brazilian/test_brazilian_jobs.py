8869c17e58f18496c617fa6f874d699596687cc3
Thales <thales.souz@outlook.com>
2023-01-24 15:31:57 -0300

    test: brazilian job test - req 11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    mock = {
+        "title": "Motorista",
+        "salary": "3000",
+        "type": "full time",
+    }
+    assert read_brazilian_file("tests/mocks/brazilians_jobs.csv")[1] == mock
